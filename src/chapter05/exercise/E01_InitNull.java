733e9dd3d8b3b769c9d2410fc0a3c716fc0eacf2
shaunZh <308826842@qq.com>
2021-11-06 12:44:06 +0800

    add: chapter02 example constructor & method overload

--- src/chapter05/exercise/E01_InitNull.java
+++ src/chapter05/exercise/E01_InitNull.java
@@ -2,7 +2,7 @@ package chapter05.exercise;
 
 public class E01_InitNull {
     String st; // 未初始化的String引用会被Java初始化为null
-    static public void main(String[] args) {
+    public static void main(String[] args) {
         E01_InitNull In = new E01_InitNull();
         System.out.println(In.st);
     }
