899f54dc0fdc6ddc6d15f00fd47a6c0a0d43fc9d
shaunZh <308826842@qq.com>
2021-11-09 06:57:26 +0800

    chapter6.1: 使用工具类

---
+++ src/chapter06/example/E1_TestPackage.java
@@ -0,0 +1,8 @@
+package chapter06.example;
+import static hexon.util.Print.*;
+
+public class E1_TestPackage {
+    public static void main(String[] args) {
+        print("test package");
+    }
+}
