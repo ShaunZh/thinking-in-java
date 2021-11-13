5d83f5e14f522d45bde958ac9d9cf404b87e93db
shaunZh <308826842@qq.com>
2021-11-13 12:38:59 +0800

    fix: E9_Foreign报错

--- src/chapter06/example/E9_PackagedClass.java
+++ src/chapter06/example/E9_PackagedClass.java
@@ -1,6 +1,6 @@
 package chapter06.example;
 
-class E9_PackagedClass { // 此处如果不加public，那么E9
+public class E9_PackagedClass { // 此处如果不加public，那么E9_PackagedClass在chapter06.exercise.E9_Foreign中无法访问
     public E9_PackagedClass() {
         System.out.println("Creating a packaged Class");
     }
